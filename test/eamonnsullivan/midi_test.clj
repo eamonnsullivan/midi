ceb650d95c68cb659004d68ecbfb76e51b330a3c
Eamonn Sullivan <eamonn.sullivan@bbc.co.uk>
2020-01-11 14:56:17 +0000

    Initial template commit

---
+++ test/eamonnsullivan/midi_test.clj
@@ -0,0 +1,7 @@
+(ns eamonnsullivan.midi-test
+  (:require [clojure.test :refer :all]
+            [eamonnsullivan.midi :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
